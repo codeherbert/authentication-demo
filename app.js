3c1ead8fd6b93e9ef41ec4746796966b2a5918c0
Bryan Herbert <bryan77@twc.com>
2019-04-10 21:37:40 +0000

    create the user model and configure passport

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express               = require("express"),
+      mongoose              = require("mongoose"),
+      passport              = require("passport"),
+      bodyParser            = require("body-parser"),
+      User                  = require("./models/user"),
+      LocalStrategy         = require("passport-local"),
+      passportLocalMongoose = require("passport-local-mongoose");
+      
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
 const app = express();
 app.set('view engine', 'ejs');
+
+app.use(require("express-session")({
+    secret: "City nighttime views are the best",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
     res.render("home");
